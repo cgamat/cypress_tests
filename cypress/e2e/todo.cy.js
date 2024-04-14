b6f8ecc6883540b1130dac5b2a80ecd52e7b9ee1
Carlos Guirado <carlosguirado23@gmail.com>
2024-04-14 22:53:00 +0200

    Tareas fin de semana 14 abril

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,1 +1,46 @@
-describe('Tarea 1 cypress',)
+describe('Agregar tareas', () => {
+it('Agregar una tarea a la lista', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/')
+    cy.get('[data-testid="text-input"]').type('Tarea 1{enter}')
+    cy.get('[data-testid="todo-item-label"]').contains('Tarea 1')
+  })
+})
+
+it('Marcar tarea como completada', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/')
+    cy.get('[data-testid="text-input"]').type('Tarea 2{enter}')
+    cy.get('[data-testid="todo-item-toggle"]').first().check
+})
+
+it('Desmarcar tarea como completada', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/')
+    cy.get('[data-testid="text-input"]').type('Tarea 3{enter}')
+    cy.get('[data-testid="todo-item-toggle"]').first().check
+    cy.get('[data-testid="todo-item-toggle"]').first().uncheck
+})
+
+it('Editar tarea', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/')
+    cy.get('[data-testid="text-input"]').type('Tarea 4{enter}')
+    cy.get('[data-testid="todo-item-label"]').dblclick()
+    cy.focused().clear().type('Tarea 4.1{enter}')
+})
+
+it('Borrar tarea',() => {
+    cy.visit('https://todomvc.com/examples/react/dist')
+    cy.get('[data-testid="text-input"]').type('Tarea 5{enter}')
+    cy.get('[data-testid="todo-item-toggle"]').first().check
+    cy.get('.clear-completed').click
+})
+
+it('Filtrar tareas', () => {
+    cy.visit('https://todomvc.com/examples/react/dist')
+    cy.get('[data-testid="text-input"]').type('Tarea 1{enter}')
+    cy.get('[data-testid="text-input"]').type('Tarea 2{enter}')
+    cy.get('[data-testid="text-input"]').type('Tarea 3{enter}')
+    cy.get('[data-testid="text-input"]').type('Tarea 4{enter}')
+    cy.get('[data-testid="text-input"]').type('Tarea 5{enter}')
+    cy.get('[data-testid="footer-navigation"] > :nth-child(3) > a').click
+    cy.get(':nth-child(2) > a').click
+    cy.get('.selected').click
+})
